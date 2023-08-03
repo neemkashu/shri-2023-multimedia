c29b1c6f233e3e80b0d10df930686de7dbd6b65b
neemkashu <sparjinkust@mail.ru>
2023-08-03 16:44:07 +0300

    feat: add svg mask and hardcode first video

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ app.get('/', function (_, res) {
 app.use('/index.css', function (_, res) {
     res.sendFile('index.css', { root: __dirname });
 });
+app.use('/drawing1.svg', function (_, res) {
+    res.setHeader('content-type', 'image/svg+xml');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.sendFile('drawing1.svg', { root: __dirname });
+});
 
 app.use('/vids', express.static('vids'));
 
